72d5914f10a8d4bbdf067e4618dd24e07100967e
Steffen Pankratz <kratz00@gmx.de>
2012-09-14 18:39:32 +0200

    - initial commit

---
+++ executable.c
@@ -0,0 +1,8 @@
+#include "library.h"
+
+int main(int argc, char **argv)
+{
+    LibraryCall();
+    return 0;
+}
+
---
+++ library.h
@@ -0,0 +1,7 @@
+#ifndef LIBRARY_H_
+#define LIBRARY_H_
+
+void LibraryCall(void);
+
+#endif // LIBRARY_H_
+
